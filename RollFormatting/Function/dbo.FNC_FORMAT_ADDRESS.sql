beb37174077d8bc3aceb797a69dabd0b1175aa1d
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2018-10-19 16:01:55 -0700

    Last Version of this project / Switch to VS database project

---
+++ RollFormatting/Function/dbo.FNC_FORMAT_ADDRESS.sql
@@ -0,0 +1,33 @@
+﻿DROP FUNCTION IF EXISTS [dbo].[FNC_FORMAT_ADDRESS]
+GO
+/*********************************************************************************************************************
+Function: FNC_FORMAT_ADDRESS
+
+Purpose: This is a common function to format owner address
+
+Parameter:
+
+Return/result: an address line
+
+Assumption: none
+
+Modified History:
+Author.....................Date...........Purpose.............Developer
+---------------------------------------------------------------------------------------------------------------------
+1.0........................Nov 2018......original build       Gerry Whitworth
+
+*********************************************************************************************************************/
+CREATE FUNCTION [dbo].[FNC_FORMAT_ADDRESS]
+(
+	@param1 int,
+	@param2 int
+)
+
+
+RETURNS varchar(max)
+AS
+BEGIN
+	DECLARE @rtnValue varchar(max)
+	SET @rtnValue = ''
+	RETURN @rtnValue
+END
